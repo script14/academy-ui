eac0194ae7647790ccb3081f4a804dadef679c54
albertkelvinhu <albertkelvinhutapea@gmail.com>
2017-07-21 17:57:46 +0700

    mengisi module reports

--- src/modules/user/report/reports.js
+++ src/modules/user/report/reports.js
@@ -9,205 +9,127 @@ import moment from "moment";
 
 @inject(Router, Dialog)
 export class reports{
-  @bindable activeIteration;
-  @bindable activeAssignment;
-  @bindable arrayData;
-  @bindable arrayUserData;
-  
-  array_data;
-  @bindable efisiensi;
 
+    @bindable dataProject;
+    @bindable dataAssignment;
+    dataTaskPerAccount;
+    @bindable dataProjectPerAccount;
+    @bindable date;
+    @bindable assignmentEfficiency;
 
-    total;    
-    sumElapsed = 0;
-    sumBudget = 0;
+        
 
     constructor(router, dialog) {
-        this.service = new RestService("core", "accounts");
-        this.iterationService = new RestService("core", "iterations");
+        this.service = new RestService("core", "accounts");    
         this.router = router; 
         this.dialog = dialog;
-        this.arrayData;
-        this.arrayUserData;
-        this.array_data;
+        this.efficiencyCount = 0;
+        this.accountId;
+        this.date;
+        
     }
 
     async activate(params){
         var id = params.id;
-        //this.assignmentService = new RestService("core", `accounts/${id}/assignments`);
-        this.data = await this.service.get(id, { filter: { include: "profile" } });
-    }
-    
-    activeIterationChanged(newValue, oldValue) {
-      this.assignmentService = new RestService("core", `iterations/${this.activeIteration.id}/assignments`);
-      this.taskService = new RestService("core", `accounts/${this.data.id}/assignments`);
-      this.assignmentTable.refresh();
-    }   
+        this.accountId = id;
+        this.data = await this.service.get(id, { filter: { include: "profile" } }); 
+        
+        this.assignmentService = new RestService("core", `accounts/${id}/assignments`);
+        this.assignmentsData = await this.assignmentService.get({ filter: { include: "task" } }); 
+        
+        this.assignmentServ = new RestService("core", `reports/account/${id}/assignments`);
+        this.totalAssignment = new RestService("core", `accounts/${id}/count/closedAssignment`);
 
-    iterationColumns = ["no",
+        this.accountEfficiency = new RestService ("core", `reports/account/${id}/assignments/efficiency`)
+        this.assignmentEfficiency = await this.accountEfficiency.get();
+
+       // this.viewAssignment = new RestService("core",`accounts/${id}/count/efficiency/${this.date}`);
+        //this.viewAssignmentReport = await this.viewAssignment.get();
+        
+    }      
+
+    assignmentsColumns = [
     {
-      field: "start", title: "date",
-      formatter: function (value, row, index) {
-        return value ? moment(value).format("DD-MMM-YYYY") : "-";
-      }
+        field: "status",
+        title: "Status"
     },
     {
-      field: "end", title: "date",
-      formatter: function (value, row, index) {
-        return value ? moment(value).format("DD-MMM-YYYY") : "-";
-      }
-    }, "remark"];
-
-
-    __iterationRowClickCallback(event) {
-      var data = event.detail;
-      this.activeIteration = data;
-
-    }
-
-    iterationLoader = (info) => {
-      var fields = this.iterationColumns.map(col => {
-        if (typeof col === "string")
-          return col;
-        else if (typeof col === "object" && col.field)
-          return col.field;
-      })
-      var loopbackFilter = createLoopbackFilterObject(info, fields)
-      return Promise
-        .all([this.iterationService.count(loopbackFilter.filter), this.iterationService.list(loopbackFilter)])
-        .then(results => {
-          var count = results[0].count;
-          var data = results[1];
-          return {
-            total: count,
-            data: data
-          };
-        });
-    };
-
-  
-
-    columns = ["status","budget","elapsed",
+      field: "task.name",
+      title: "Task Name"
+    },
+    {
+      field: "task.type",
+      title: "Task Type"
+    },  
+    {
+        field: "budget",
+        title: "Budget Time"
+    },
+    {
+        field: "elapsed",
+        title: "Elapsed Time"
+    },
     {
         field: "date", 
-        title: "date",
+        title: "Date",
         formatter: function (value, row, index) {
-        return value ? moment(value).format("DD-MMM-YYYY") : "-";
-    }}, 
-    "remark"];
+        return value ? moment(value).format("DD-MMM-YYYY") : "-";}
+    },
+    {
+        field: "remark",
+        title: "Remark"
+    }];
 
-    assignmentLoader = (info) => {
-      if (!this.activeIteration)
-        return Promise.resolve({ total: 0, data: [] });
-      else{
-        var fields = this.columns.map(col => {
-        if (typeof col === "string")
-          return col;
-        else if (typeof col === "object" && col.field)
-          return col.field;
-      })
-      var loopbackFilter = createLoopbackFilterObject(info, fields)
-      return Promise
-        .all([this.assignmentService.count(loopbackFilter.filter), this.assignmentService.list(loopbackFilter)])
-        .then(results => {          
-          var count = results[0].count;
-          var data = results[1];
-          this.arrayData = results[1];
-          this.total= count;
-
-          this.userAssignmentTable.refresh();
-          this.userIterationAssignmentTable.refresh();
-          return {
-            total: count,
-            data: data
-          };
-        });     
-      }  
+    __dateFormatter = function (value, row, index) {
+    return value ? moment(value).format("DD-MMM-YYYY") : "-";
     }
 
-    userAssignmentLoader = (info) => {
-      if (!this.activeIteration)
-        return Promise.resolve({ total: 0, data: [] });
-      else{
-        var fields = this.columns.map(col => {
-        if (typeof col === "string")
-          return col;
-        else if (typeof col === "object" && col.field)
-          return col.field;
-      })
-      var loopbackFilter = createLoopbackFilterObject(info, fields)
-      return Promise
-        .all([this.taskService.count(loopbackFilter.filter), this.taskService.list(loopbackFilter)])
-        .then(results => {
-          var count = results[0].count;
-          var data = results[1];
+    taskColumns = [
+    "code",
+    "name",
+    {
+      field: "date", title: "Date",
+      formatter: this.__dateFormatter
+    },
+    "budget",
+    "actual",
+    {
+      field: "open", title: "Open",
+      formatter: this.__dateFormatter
+    },
+    {
+      field: "close", title: "Close",
+      formatter: this.__dateFormatter
+    },
+    "remark",
+    "status"];
 
-          //array of JSON 'result[0]' dipanggil, dan disimpan dalam arrauUserData
-          this.arrayUserData = results[1];
-          this.total= count;          
-          
-          //memanggil method refresh tabel userIterationAssignmentTable
-          this.userIterationAssignmentTable.refresh();
-          return {
-            total: count,
-            data: data
-          };
-        });     
-      }  
-    }
 
-    userIterationAssignmentLoader = (info) => {
-      if (!this.activeIteration)
-        return Promise.resolve({ total: 0, data: [] });
-      else{
-        var fields = this.columns.map(col => {
-        if (typeof col === "string")
-          return col;
-        else if (typeof col === "object" && col.field)
-          return col.field;
-      })
-      var loopbackFilter = createLoopbackFilterObject(info, fields)
-      return Promise
-        .all([this.arrayData, this.arrayUserData])
-        .then(results => {
-            var data_a = results[0];  
-            console.log("data_a dipanggil")
-            console.log(data_a);
-            var data_b = results[1];
-            console.log("data_b dipanggil")
-            console.log(data_b);
-            
-            for(var i = 0; i < data_a.length; i++) {
-              for(var j = 0; j < data_b.length; j++) {  
-                  var obj_a = data_a[i];
-                  var obj_b = data_b[j];
-                  console.log(obj_a.id);
-                  console.log(obj_b.id);
-                  if(data_a[i]=data_b[j]){
-                    this.array_data = data_b;
-                    this.arrayData = 0;
-                    this.arrayUserData = 0;
-                  }
-              }
-            }
-          return {
-            data: this.array_data
-          };
+  assignmentLoader = (info) => {
+    var fields = this.assignmentsColumns.map(col => {
+      if (typeof col === "string")
+        return col;
+      else if (typeof col === "object" && col.field)
+        return col.field;
+    })
+    var loopbackFilter = createLoopbackFilterObject(info, fields)
+    return Promise
+      .all([this.assignmentService.count(loopbackFilter.filter),this.assignmentsData])
+      .then(results => {
+        var count = results[0].count;
+        var data = results[1];
+        console.log(data);
 
-        });     
-      }  
-    }
+        this.getEfficiency();
+        return {
+          total: count,
+          data: data
+        };
+      });
+    };
 
-  calculateSum() {
-    let sumElapsed = 0;
-    let sumBudget = 0;
-    for (let item of this.array_data) {
-      sumElapsed = sumElapsed + parseFloat(item.elapsed);
-      sumBudget  = sumBudget + parseFloat(item.budget);
-    }
-    this.sumElapsed = sumElapsed;
-    this.sumBudget = sumBudget;
-    this.efisiensi = parseFloat(this.sumElapsed)/this.sumBudget;
-    this.efisiensi = this.efisiensi*100;
+  getEfficiency(){
+    this.efficiencyCount = this.assignmentEfficiency.efficiency;
+    //nilai efficiencyCount adalah array yang memiliki [nilai elapsed, nilai budget, nilai efisiensi]
   }
 }
