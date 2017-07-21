1f5cd526968f0cee2c5d03ab45e6c9f8a610aaca
albertkelvinhu <albertkelvinhutapea@gmail.com>
2017-07-21 17:21:12 +0700

    getEfficiency mengambil budget, elapsed, dan efficiency  di dalam satu variabel array

--- src/modules/user/projects/projects.js
+++ src/modules/user/projects/projects.js
@@ -132,7 +132,7 @@ export class projects{
   getEfficiency(){
     console.log("getting efficiency.");
     this.efficiencyCount = this.assignmentEfficiency.efficiency;
-    console.log(this.assignmentEfficiency.efficiency);
+    //nilai efficiencyCount adalah array yang memiliki [nilai elapsed, nilai budget, nilai efisiensi]
     console.log("done getting efficiency.")
   }
 
